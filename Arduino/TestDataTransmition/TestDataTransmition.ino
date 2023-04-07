23322e9d81e21c46506914db901a2cae3c070166
Dan Glasson <dglasson18@gmail.com>
2023-04-08 09:55:38 +1000

    Added an Arduino ino to test data transmission from an arduino. Modified Controller.py to include functions for receiving and confirming valid messages from other network devices. Functions were also added to Controller.py to update real time database, but are not yet implemented

---
+++ Arduino/TestDataTransmition/TestDataTransmition.ino
@@ -0,0 +1,94 @@
+#include <SoftwareSerial.h>
+
+//SoftwareSerial XBee(1,0); // RX, TX
+SoftwareSerial XBee(2,3); // RX, TX
+
+#include "DHT.h"
+
+#define DHTPIN 7
+#define DHTTYPE DHT11
+
+
+int send_data(String DevID, String DevDataType, String DevData);
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  XBee.begin(9600);
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  float temp = dht.readTemperature(); // read temperature data
+  float humi = dht.readHumidity();
+  // Send data to the coordinator'
+  //Serial.print(temp);
+  send_data("002", "TEMP", String(temp));
+  delay(100);
+  send_data("002", "HUMI", String(humi));
+  // XBee.print("002HUMI");
+  // XBee.println(dht.readHumidity());
+  delay(5000);
+}
+
+int send_data(String DevID, String DevDataType, String DevData) {
+  //do some rudimentary checking to ensure input parameters conform to messaging standard
+  if (DevID.length() != 3) {
+    //Serial.print("DevID Contains too many characters");
+    return 3;
+  }
+  if (DevDataType.length() != 4) {
+    //Serial.print("Error in DevDataType, invalid length");
+    return 3;
+  }
+  for (int i = 0; i <= 2; i++) {
+    if ((DevID[i] < '0') || (DevID[i] > '9')) {
+      //Serial.print("DevID Contains invalid characters");
+      return 3;
+      //valid DevID
+    }
+  }
+  for (int i = 0; i <= 3; i++) {
+    if (((DevDataType[i] < 'A') || (DevDataType[i] > 'z')) || ((DevDataType[i] > 'Z') && (DevDataType[i] < 'a')))
+    {
+      //Serial.print("Error in DevDataType, invalid character");
+      //Serial.println(DevDataType[i]);
+      return 3;
+    }
+  }
+  for (int i = 0; i < DevData.length(); i++)
+  {
+    if ((DevData[i] < '0' || DevData[i] > '9') && (DevData[i] != '.')) {
+      //Serial.print("DevData contains invalid characters");
+      //Serial.println(DevData[i]);
+      return 3;
+    }
+  }
+  String Message = DevID + DevDataType + DevData;
+  if (XBee.available())
+    XBee.readString();
+  XBee.println(Message);
+  delay(100);
+  for (int i = 0; i < 10; i++) {
+    if (XBee.available()) {
+      String XBeeConfirmation = XBee.readString();
+      Serial.println(XBeeConfirmation.length()); 
+      Serial.println(XBeeConfirmation);  
+      if (XBeeConfirmation == Message)
+      {
+        //Serial.println("Confirmed");
+        return 0;
+      }
+    }
+    Serial.print("No message available in response to - ");
+    Serial.println(Message);
+        //Serial.println(Message);
+        delay(200);
+        XBee.readString();
+        XBee.println(Message);
+        delay(100);
+  }
+  Serial.println("No confirmation of transmission was received");
+  return 1;
+}
--- RPi/Controller.py
+++ RPi/Controller.py
@@ -1,1 +1,166 @@
-#Placeholder for python script to run on Raspberry Pi
+import pyrebase
+from firebase import firebase
+import serial
+import time
+from datetime import datetime
+import re
+
+ser = serial.Serial('/dev/ttyUSB0', 9600) # configure Xbee port
+
+config = {
+'apiKey': "AIzaSyC6MlFy4Btor9bQ4a_RHh1fJhuefeWwjAo",
+'authDomain': "rpitest2-263a3.firebaseapp.com",
+'databaseURL': "https://rpitest2-263a3-default-rtdb.firebaseio.com/",
+'projectId': "rpitest2-263a3",
+'storageBucket': "rpitest2-263a3.appspot.com",
+'messagingSenderId': "712072402216"
+}
+
+#Initialise firebase
+firebase = pyrebase.initialize_app(config)
+#Get a reference to the database service
+db = firebase.database()
+
+def update_firebase_temperature(temperature, current_time, label):
+        if temperature is not None:
+                data = {"label":current_time, "value":temperature}
+                results = db.child("Temperature").child(label).set(data)
+                time.sleep(4)
+        else:
+                print('Failed to get reading. Try Again!')
+                time.sleep(4)
+
+def update_firebase_humidity(humidity, current_time, label):
+        if humidity is not None:
+                data = {"label":current_time, "value":humidity}
+                results = db.child("Humidity").child(label).set(data)
+                time.sleep(4)
+        else:
+                print('Failed to get reading. Try Again!')
+                time.sleep(4)
+
+def update_firebase_light(light, current_time, label):
+        if light is not None:
+                data = {"label":current_time, "value":light}
+                results = db.child("Light").child(label).set(data)
+                time.sleep(4)
+        else:
+                print('Failed to get reading. Try Again!')
+                time.sleep(4)
+
+def update_firebase_room_occupancy(room_occupancy, current_time, label):
+        if room_occupancy is not None:
+                data = {"label":current_time, "value":room_occupancy}
+                results = db.child("Room_Occupancy").child(label).set(data)
+                time.sleep(4)
+        else:
+                print('Failed to get reading. Try Again!')
+                time.sleep(4)
+
+# PSEUDOCODE
+#  -  Read input from XBEE using the pyserial library, one byte at a time to enable easier input validation
+#  -  validate the input based on the expected sensors and format of messages being received
+#  -  If valid
+#  -    Broadcast received message as means of validating receipt (not pretty, but should do the trick)
+#  -    return Device ID, Device Data Type (e,g, Temp, light, etc.) and Device Data Value
+#  -  If Invalid
+#  -    return False
+# Assumptions
+#  - Messages transmitted by devices on the network will be of the form \d{3}[A-Z]{4}[0-9.]+ i.e. 3 digits (device id), 4 characters (device data type), at least one integer or float value (device data)
+# Example Output (Console) - Successful reception of valid message
+#	<re.Match object; span=(0, 3), match='002'>
+#	<re.Match object; span=(3, 7), match='HUMI'>
+#	Device ID:  002
+#	Device Data Type:  HUMI
+#	Device Data:  46.00
+#	12
+#	Confirmation sent 2023-04-08 09:44:26.236539
+# Example Output (Return Value) - Successful reception of valid message
+#	Message = ('001', 'TEMP', '34.58')
+def read_XBEE():
+	Data = ""
+	ValidMessage = True
+	# SensorData = ser.read()
+	while ValidMessage:
+		c_char = ''
+		for c in ser.read(1):
+			c_char = chr(c)
+		if c_char == "\n" or c_char == "\r":
+			break
+		if (len(Data) <= 2 and (c_char < "0" or c_char > "9")):
+			return False
+		Data += c_char
+		if len(Data) == 7:
+			DevID = re.search("^\d{3}", Data)
+			DevDataType = re.search("(?<=^\d{3})[A-Z]{4}", Data)
+			if (DevID is None or DevDataType is None):
+				print("*****************************************************")
+				return False
+			# 	#Regex matches
+	DevID = re.search("^\d{3}", Data)
+	print(DevID)
+	DevDataType = re.search("(?<=^\d{3})[A-Z]{4}", Data)
+	print(DevDataType) 
+	DevData = re.search("(?<=^\d{3}[A-Z]{4})([0-9.]+)$", Data)
+	#(?<=^b'\d{3}[A-Z]{4})([0-9.]+)(?=\\\\r\\\\n'$)
+	if DevID and DevDataType and DevData:
+		print("Device ID: ", DevID.group(0))
+		print("Device Data Type: ", DevDataType.group(0))
+		print("Device Data: ", DevData.group(0))
+		string_encode = (DevID.group(0) + DevDataType.group(0) + DevData.group(0)).encode('utf-8')
+		print(ser.write(string_encode)) #send data to Arduino
+		print("Confirmation sent", datetime.now())
+		return DevID.group(0), DevDataType.group(0), DevData.group(0)
+	else:
+		ValidMessage = False
+	return False	
+
+	# Following reads input from serial one line at a time, but can be more prone to being unable to validate message when more then one message is received
+	# in quick succession, where one of the messages does not terminate with a line feed (i.e. the two messages are concatenated)
+	# can probably be removed at some point, was just part of the first draft
+	# ser.flushInput()
+	# #SensorData is read in as bytes, convert to string for easier processing with regex
+	# if SensorData != None:
+	# 	Data = str(SensorData)
+	# 	print(Data)
+	# 	#When converted to string, message is of the form b'message\r\n' (when sent with Xbee.println)
+	# 	#Regex matches
+	# 	DevID = re.search("(?<=^b')\d{3}", Data)
+	# 	print(DevID)
+	# 	DevDataType = re.search("(?<=^b'\d{3})[A-Z]{4}", Data)
+	# 	print(DevDataType) 
+	# 	DevData = re.search("(?<=^b'\d{3}[A-Z]{4})([0-9.]+)(?=\\\\r\\\\n'$)", Data)
+	# 	#(?<=^b'\d{3}[A-Z]{4})([0-9.]+)(?=\\\\r\\\\n'$)
+	# 	print(DevData)
+	# 	#Confirm that message was appropriately formatted, and that DevID, DevDataType and DevData were able to be extracted from the message
+	# 	if DevID and DevDataType and DevData:
+	# 		print("Device ID: ", DevID.group(0))
+	# 		print("Device Data Type: ", DevDataType.group(0))
+	# 		print("Device Data: ", DevData.group(0))
+	# 		#----- send a confirmation ------
+	# 		#string = "Data received"
+	# 		#string_encode = Data.encode() #encode to right format
+	# 		string_encode = (DevID.group(0) + DevDataType.group(0) + DevData.group(0)).encode('utf-8')
+	# 		print(ser.write(string_encode)) #send data to Arduino
+	# 		print("Confirmation sent", datetime.now())
+	# 		return DevID.group(0), DevDataType.group(0), DevData.group(0)
+	# 	else:
+	# 		return None
+	# else:
+	# 	return None
+
+
+# Continuously read and print data
+while True:
+	try:
+
+		Message = read_XBEE()
+		if Message != False:
+			print(Message)
+		else:
+			print("Invalid input received and dropped")
+	except KeyboardInterrupt:
+		break
+
+ser.close()
+print("done")
