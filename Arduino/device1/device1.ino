eae10d7f2f98aefe854c072a93ad37a2aed31c9f
Dan Glasson <dglasson18@gmail.com>
2023-04-13 23:09:49 +1000

    Modified device 1 to be able to interpret mutltiple messages, e.g. confirmation of previously sent message followed by an isntruction, to allow it to turn on an LED

--- Arduino/device1/device1.ino
+++ Arduino/device1/device1.ino
@@ -1,30 +1,59 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SoftwareSerial.h>
 
+//SoftwareSerial XBee(1,0); // RX, TX
+SoftwareSerial XBee(2,3); // RX, TX
+
+#include "DHT.h"
+
+#define DHTPIN 7
+#define DHTTYPE DHT11
+
+#define LEDPIN 8
+
+String DeviceID = "D01";
+
+int send_data(String DevID, String DevDataType, String DevData);
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  XBee.begin(9600);
+  Serial.begin(9600);
+  pinMode(LEDPIN, OUTPUT);
+  randomSeed(analogRead(5));
+  dht.begin();
 }
 
 void loop() {
   float temp = dht.readTemperature(); // read temperature data
   // add Header
-  tempMsg = "TEMP"+temp
+  String tempMsg = DeviceID + "TEMP"+ String(temp);
   // Send data to the coordinator
-  XBee.print(tempMsg);
-  
+  if (XBee.available() )
+    XBee.readString();
+  XBee.println(tempMsg);
+  delay(350);
   if (XBee.available()) // If data comes in from XBee, we process the data
-{
-  rawRead = XBee.read(); // read message from XCTU
-  header = rawRead.substrig(0,3) // obtain the message header
-  if (header = "LEDP")
   {
-    if (rawRead.charAt(4) = "H")
-    {
-      digitalWrite( //pin1// ,HIGH)
-      digitalWrite( //pin2// ,HIGH)
-    }
-        if (rawRead.charAt(4) = "L")
+    String rawRead = XBee.readString(); // read message from XCTU
+    String header = rawRead.substring(0,3);
+    Serial.println(header == DeviceID);
+    int nextD = 0;
+    for (int i = 0; i < rawRead.length() -3; i++)
+    {      
+      if (String(String(rawRead[i]) + String(rawRead[i+1]) + String(rawRead[i+2])) == header)
+      {      
+        Serial.println("Found message");
+        if (rawRead[i+7] == 'H')
         {
-      digitalWrite( //pin1// ,LOW)
-      digitalWrite( //pin2// ,LOW)
+          digitalWrite(LEDPIN ,HIGH);
+        }
+        if (rawRead[i+7] == 'L')
+        {
+          digitalWrite(LEDPIN ,LOW);
+        }
+      }
     }
   }
+delay(1000);  
 }
